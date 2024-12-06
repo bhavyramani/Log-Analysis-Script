c4397ddfe8067ca09d359d0a68926035e2b80044
Bhavy Ramani <69421006+bhavyramani@users.noreply.github.com>
2024-12-06 22:08:49 +0530

    Count IP Address and print in descending order of count

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def parse_input(line):
+# Funtion to convert String line input to Readable Dictionary
+def parse_input(line):  
     line = line.strip()
     line = line.strip('\n')
     line = line.split(' ')
@@ -9,7 +10,7 @@ def parse_input(line):
     data['datetime'] = line[3] + ' ' + line[4]
     data['request_type'] = line[5].strip('"')
 
-    data['url'] = line[6]
+    data['endpoint'] = line[6]
     data['protocol'] = line[7].strip('"')
     data['status_code'] = line[8]
     data['response_size'] = line[9]
@@ -19,14 +20,83 @@ def parse_input(line):
         
     return data
 
+
+# Function to write output to file and console
+def write_output(data, pad_size = -1):
+    # Padding data with spaces or print new line
+    if pad_size == -1:
+        data = data + '\n'
+    else:
+        data = data.ljust(pad_size)
+    output_file.write(data)
+    print(data, end = '')
+    
+    
+ip_count = dict()              # Dictionary to store IP count
+end_point_count = dict()       # Dictionary to store Endpoint count
+brute_force_attepmts = dict()  # Dictionary to store Brute Force Attempts
+brute_force_threshold = 10     # Threshold to for Brute Force Detection (default : 10)
+
+
+# Process input data
+def process_count(data):
+    # Using global variables inside function
+    global ip_count, most_freq_ap, brute_force_attepmts
+    
+    # Logic to conut IP address
+    if data['ip'] in ip_count:
+        ip_count[data['ip']] += 1
+    else:
+        ip_count[data['ip']] = 1
+        
+    # Logic to store count of each endpoint
+    if data['endpoint'] in end_point_count:
+        end_point_count[data['endpoint']] += 1
+    else:
+        end_point_count[data['endpoint']] = 1
+    
+    # Logic to detect Brute Force Attempts
+    if data['status_code'] == '401' or ('message' in data and data['message'] == 'Invalid credentials'):
+        if data['ip'] in brute_force_attepmts:
+            brute_force_attepmts[data['ip']] += 1
+        else:
+            brute_force_attepmts[data['ip']] = 1
+            
+            
+# Function to print IP counts as per output format
+def print_ip_count():
+    pad_size = 20
+    
+    # Sort IP addresses based on descending order of count
+    ip_count_list = []
+    for ip, count in ip_count.items():
+        ip_count_list.append((ip, count))
+    ip_count_list = sorted(ip_count_list, key=lambda x: x[1], reverse=True)
+    
+    write_output('IP Address,', pad_size)
+    write_output('Request Count')
+    
+    for ip, count in ip_count_list:
+        write_output(ip + ',', pad_size)
+        write_output(str(count))
+
+    write_output('\n\n') # Seperator for next output
+
 if __name__ == '__main__':
-    with open('sample.log') as f:
-        lines = f.readlines()
-        
-        all_data = []
-        
+    global output_file
+    output_file = open('log_analysis_results.csv', 'w')
+    
+    # Read input file line by line so it can handle large files
+    with open('sample.log') as f:   
+        lines = f.readlines()        
         for line in lines:
-            parsed_line = parse_input(line)
-            all_data.append(parsed_line)
-        
-        print(all_data)
+            # Parsing the input line to dictionary
+            data = parse_input(line)
+            
+            # Process data of current line
+            process_count(data)
+            
+        # Calling main functionalities
+        print_ip_count()
+    
+    output_file.close()
